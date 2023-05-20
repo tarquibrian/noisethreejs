8c625db31d7354028f86c669795568be4e8d5191
brian tarqui <tarquibrian@gmail.com>
2023-05-20 14:58:54 -0400

    completed animation render

--- src/threejs/Blob.tsx
+++ src/threejs/Blob.tsx
@@ -8,9 +8,6 @@ const Blob = () => {
   const mesh = useRef();
   const uniforms = useMemo(
     () => ({
-      // u_time: {
-      //   value: 0.0,
-      // },
       color1: {
         value: new THREE.Color("#ebf4f5"),
       },
@@ -22,17 +19,9 @@ const Blob = () => {
   );
 
   useFrame(() => {
-    // const { clock } = state;
-    // console.log(clock);
-    // mesh.current.material.uniforms.time.value += 0.007;
-    // mesh.current.rotateZ(0.004);
     mesh.current.rotation.z += 0.004;
     mesh.current.rotation.x += 0.004;
     mesh.current.rotation.y += 0.004;
-    // updateColor(valor)
-    // setTimeout(() => {
-    //   updateColor(100);
-    // }, 2000);
   });
 
   return (
