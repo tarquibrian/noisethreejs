b6f8821c4e26acaf47498f8421e55476cb2208e4
brian tarqui <tarquibrian@gmail.com>
2023-05-20 14:59:32 -0400

    config framgment shader

--- src/threejs/shaders/fragment.ts
+++ src/threejs/shaders/fragment.ts
@@ -1,11 +1,11 @@
 export const fragment = `
-uniform vec3 color1;
-        uniform vec3 color2;
-      
-        varying vec2 vUv;
-        
-        void main() {
-          
-          gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
-        }
+	uniform vec3 color1;
+	uniform vec3 color2;
+
+	varying vec2 vUv;
+	
+	void main() {
+		
+		gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+	}
 `;
