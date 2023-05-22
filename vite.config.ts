1023c998b2f4f2129e28077d19a20858d3543a51
brian tarqui <tarquibrian@gmail.com>
2023-05-21 22:05:16 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/effect-threejs/',
+  base: '/noisethreejs/',
   plugins: [react()],
 })
